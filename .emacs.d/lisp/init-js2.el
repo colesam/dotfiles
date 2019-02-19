b6ce8d53d08cdbf2cb7261427263eb59e59f2614
Samuel Cole <samuel@myglobaldata.com>
2019-02-19 15:28:17 -0600

    Add electric pair and multiline declarations to js2 mode

--- .emacs.d/lisp/init-js2.el
+++ .emacs.d/lisp/init-js2.el
@@ -8,6 +8,9 @@
   :init
   (setq js-indent-level 2)
   (setq js2-strict-missing-semi-warning nil)
+  (setq js2-pretty-multiline-declarations 'dynamic)
+  :hook
+  (js2-mode . electric-pair-mode)
 
   :mode ("\\.js$" . js2-mode))
 
